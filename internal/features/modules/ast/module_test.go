7dfe82defe9fb87e67417f3c2d5f447caa9f12b6
Lumbenlengo <patriciolumbee@gmail.com>
2026-09-23 09:42:02 +0000

    test: cover .tofu.json module filename
    Signed-off-by: Lumbenlengo <patriciolumbee@gmail.com>

--- internal/features/modules/ast/module_test.go
+++ internal/features/modules/ast/module_test.go
@@ -50,6 +50,7 @@ func TestIsModuleFilename(t *testing.T) {
 		{"main.tf", true},
 		{"main.tofu", true},
 		{"main.tf.json", true},
+		{"main.tofu.json", true},
 		{"main.txt", false},
 		{"main.json", false},
 		{".hidden.tf", true},
